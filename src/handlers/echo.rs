571ffcc1f28541602b3a3a7f803d452e92779ca9
afresquet <29437693+afresquet@users.noreply.github.com>
2025-01-09 15:01:41 +0100

    multiple compression schemes

--- src/handlers/echo.rs
+++ src/handlers/echo.rs
@@ -3,11 +3,17 @@ use codecrafters_http_server::{Body, Request, Response};
 pub fn handler(request: Request) -> Response {
     let body = request.target.split('/').last().expect("has body to echo");
     let mut respose_builder = Response::builder().body(Body::TextPlain(body.to_string()));
-    match request.headers.get("Accept-Encoding") {
-        Some(header) if header == "gzip" => {
+
+    let accept_encoding_header = request
+        .headers
+        .get("Accept-Encoding")
+        .map(|header| header.split(", ").collect::<Vec<_>>());
+    match accept_encoding_header {
+        Some(compression_schemes) if compression_schemes.contains(&"gzip") => {
             respose_builder = respose_builder.header("Content-Encoding", "gzip");
         }
         _ => (),
     }
+
     respose_builder.build()
 }
